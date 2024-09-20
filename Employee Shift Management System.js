76fa2628abd4888cd5df638513ceb0b3b56dcd2f
ramishahq <hoque2@usf.edu>
2024-09-20 01:24:31 -0400

    "Initialize employees with shift schedules"

---
+++ Employee Shift Management System.js
@@ -0,0 +1,7 @@
+//Task 1 Create an Employees Array of Employee Objects
+let employeesDetails = [
+    { name: 'Fahmid', day: 'Monday', hours: 6},
+    { name: 'Sadman', day: 'Tuesday', hours: 7},
+    { name: 'Raiyan', day: 'Wednesday', hours: 8},
+    { name: 'Nayeem', day: 'Thursday', hours: 9}
+];
