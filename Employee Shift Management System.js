5b081356683e104a8d22f343b4a7ea7b25492236
ramishahq <hoque2@usf.edu>
2024-09-20 01:42:55 -0400

    "Create displayEmployeeShifts function"

--- Employee Shift Management System.js
+++ Employee Shift Management System.js
@@ -5,3 +5,18 @@ let employeesDetails = [
     { name: 'Raiyan', day: 'Wednesday', hours: 8},
     { name: 'Nayeem', day: 'Thursday', hours: 9}
 ];
+
+// Task 2 Create a Function to Display Employee Shift Details:
+function displayEmployeeShifts (employeeDetail){
+    console.log (`Employee Name:${employeeDetail.name}` );
+    console.log (`Works Day Shift :${employeeDetail.day}, Hours worked ${employeeDetail.hours}`);
+    
+}
+// Testing function of displayEmployeeShifts
+employeesDetails.forEach(displayEmployeeShifts); 
+
+//output 
+//Employee Name:Fahmid- Works Day Shift :Monday, Hours worked 6
+//Employee Name:Sadman- Works Day Shift :Tuesday, Hours worked 7
+//Employee Name:Raiyan- Works Day Shift :Wednesday, Hours worked 8
+//Employee Name:Nayeem- Works Day Shift :Thursday, Hours worked 9
